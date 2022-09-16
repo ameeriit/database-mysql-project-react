506029cbd393d40bc5e3e2f859c7a02c701301dd
@meeriit <amritpokharel51@gmail.com>
2022-09-16 12:38:06 +0545

    zz

---
+++ src/screens/OwnerCurrentBookings/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+
+import "./styles.css";
+
+const OwnerActive = (props) => {
+   const [items, setItems] = React.useState([]);
+   React.useEffect(async () => {
+      const obj = {
+         fieldID: props.fieldID
+      };
+      const config = {
+         headers: {
+         'Content-Type': 'application/x-www-form-urlencoded'
+         }
+     };
+      await axios.post('https://dbproject-group21.000webhostapp.com/db_proj/OActiveBookings.php', obj, config).then((res) => {
+         setItems(res.data);
+      });
+   }, []);
+
+   return (
+      <div className="container">
+         <h1>Active Bookings</h1>
+         {items.map((i) => (
+            <div className="item">
+               <div>
+                  <div>Customer Name : {i.name}</div>
+                  <div className="zahn">
+                     <div>Timings : {i.start_time}</div>
+                     <div>Date : {i.date}</div>
+                  </div>
+               </div>
+            </div>
+         ))}
+      </div>
+   );
+};
+
+export default OwnerActive;
